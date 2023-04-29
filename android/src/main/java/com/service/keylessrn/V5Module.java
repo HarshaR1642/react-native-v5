86380cc0ae3107b89eb9c1d06a0bbaf4e57136ec
HarshaR1642 <sriharsha@rently.com>
2023-04-30 00:39:23 +0530

    removed unwanted imports

--- android/src/main/java/com/service/keylessrn/V5Module.java
+++ android/src/main/java/com/service/keylessrn/V5Module.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
